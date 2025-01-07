af9d50e7052cb1e993008ad373f7588289fb73d4
egenoor <egenoor@users.noreply.github.com>
2025-01-07 18:17:35 +0200

    Add word calculation method in service

--- src/main/java/com/ege/scrabble/controller/GameController.java
+++ src/main/java/com/ege/scrabble/controller/GameController.java
@@ -1,7 +1,19 @@
 package com.ege.scrabble.controller;
 
+import com.ege.scrabble.service.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GameController {
+    @Autowired
+    GameService gameService;
+
+    @PostMapping("/api/scrabble/calculate")
+    public int calculateWord(@RequestParam String word) {
+        gameService.addLetterValues();
+        return gameService.calculateWord(word);
+    }
 }
--- src/main/java/com/ege/scrabble/service/GameService.java
+++ src/main/java/com/ege/scrabble/service/GameService.java
@@ -1,7 +1,56 @@
 package com.ege.scrabble.service;
 
 import org.springframework.stereotype.Service;
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Service
 public class GameService {
+    Map<String, Integer> letterValues;
+
+    public void addLetterValues() {
+        letterValues = new HashMap<>() {{
+            put("E", 1);
+            put("A", 1);
+            put("I", 1);
+            put("O", 1);
+            put("N", 1);
+            put("R", 1);
+            put("T", 1);
+            put("L", 1);
+            put("S", 1);
+            put("U", 1);
+            put("D", 2);
+            put("G", 2);
+            put("B", 3);
+            put("C", 3);
+            put("M", 3);
+            put("P", 3);
+            put("F", 4);
+            put("H", 4);
+            put("V", 4);
+            put("W", 4);
+            put("Y", 4);
+            put("K", 5);
+            put("J", 8);
+            put("X", 8);
+            put("Q", 10);
+            put("Z", 10);
+        }};
+    }
+
+    public int calculateWord(String word) {
+        int score = 0;
+        String[] splitWord = word.split("");
+
+        for(String c : splitWord) {
+            //TODO: check if word exists in dictionary
+            if (letterValues.containsKey(c.toUpperCase())) {
+                score += letterValues.get(c.toUpperCase());
+            }
+        }
+
+        return score;
+    }
 }
